53a8a2c2bb4bbc16b7c68d3e4002313d1e59db00
Lohann Paterno Coutinho Ferreira <developer@lohann.dev>
2024-12-16 18:56:44 -0300

    Remove forge import from Primitives.sol

--- src/Primitives.sol
+++ src/Primitives.sol
@@ -6,7 +6,6 @@ pragma solidity >=0.8.0;
 import {BranchlessMath} from "./utils/BranchlessMath.sol";
 import {UFloatMath, UFloat9x56} from "./utils/Float9x56.sol";
 import {NetworkID} from "./NetworkID.sol";
-import {console} from "forge-std/Test.sol";
 
 /**
   * @dev GMP message EIP-712 Type Hash.
